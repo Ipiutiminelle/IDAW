23b19c270f6deeffad03b45c1668cc4874433eb5
Géry <gery.bellanger@gmail.com>
2023-10-10 09:40:42 +0200

    Code PHP qui affiche l'heure

---
+++ TP2-PHP/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        
+    
+<?php
+// Set the new timezone
+date_default_timezone_set('Europe/Paris');
+$date = date('d-m-y h:i:s');
+
+Print("Nous sommes le $date");
+?>
+
+</body>
+
+</html>
