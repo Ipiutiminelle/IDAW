ebe78386f255e1d18a01e15f259ccfecf1033986
Géry <gery.bellanger@gmail.com>
2023-10-11 09:17:34 +0200

    1.4

---
+++ TP3/connected.php
@@ -0,0 +1,12 @@
+<?php
+
+$test_login=$_GET['login'];
+$test_password=$_GET['password'];
+
+
+echo "<h1> Bienvenue ".$test_login. "</h1>";
+echo "<h2> Ton mot de passe était : ".$test_password."<h2>"
+
+?>
+
+<!-- réponse question 4) => On remarque que le login et le password sont dans l'URL -->
