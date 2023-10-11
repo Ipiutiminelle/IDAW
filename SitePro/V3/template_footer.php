d7cdca0621439f4b5c28a97669b72d8595b24306
Géry <gery.bellanger@gmail.com>
2023-10-11 09:25:24 +0200

    Rajout </html> à la fin du footer

--- SitePro/V3/template_footer.php
+++ SitePro/V3/template_footer.php
@@ -1,6 +1,6 @@
-
-
-    <footer class="footer">
-<p class="glow">Copybright</p>
-<a href="index.php?page=info_technique"> Infos techniques </a>
+<footer class="footer">
+    <p class="glow">Copybright</p>
+    <a href="index.php?page=info_technique"> Infos techniques </a>
 </footer>
+
+</html>
