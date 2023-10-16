1e9f098ca9036473dc3b6dbaec8acf5366633af5
Géry <gery.bellanger@gmail.com>
2023-10-16 10:13:20 +0200

    petite pause

---
+++ tp4/exo3/init_db.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('config.php');
+$connectionString = "mysql:host=" . _MYSQL_HOST;
+if (defined('_MYSQL_PORT'))
+    $connectionString .= ";port=" . _MYSQL_PORT;
+$connectionString .= ";dbname=" . _MYSQL_DBNAME;
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+$pdo = NULL;
+try {
+    $pdo = new PDO($connectionString, _MYSQL_USER, _MYSQL_PASSWORD, $options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $erreur) {
+    echo 'Erreur : ' . $erreur->getMessage();
+}
+
+$sql = file_get_contents('sql/create_db.sql');
+
+if ($pdo->exec($sql) !== false) {
+    echo "<p> La base de données a été créée avec succès.</p>";
+} else {
+    echo "<p>Erreur lors de la création de la base de données.</p>";
+}
+
+?>
---
+++ tp4/exo3/sql/create_db.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : lun. 16 oct. 2023 à 07:11
+-- Version du serveur : 5.7.39
+-- Version de PHP : 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `dbtest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`) VALUES
+(1, 'Nom1', 'email1@example.com'),
+(2, 'Nom2', 'email2@example.com'),
+(3, 'Nom3', 'email3@example.com'),
+(4, 'Nom4', 'email4@example.com'),
+(5, 'Nom5', 'email5@example.com'),
+(6, 'Nom6', 'email6@example.com'),
+(7, 'Nom7', 'email7@example.com'),
+(8, 'Nom8', 'email8@example.com'),
+(9, 'Nom9', 'email9@example.com'),
+(10, 'Nom10', 'email10@example.com'),
+(11, 'Nom11', 'email11@example.com'),
+(12, 'Nom12', 'email12@example.com'),
+(13, 'Nom13', 'email13@example.com'),
+(14, 'Nom14', 'email14@example.com'),
+(15, 'Nom15', 'email15@example.com');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
